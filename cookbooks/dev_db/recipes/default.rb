8c9e0068b5833a056c7f0d8ad2068db1e7e0fa2d
Simon Luijk <simon@simonluijk.com>
2013-11-15 19:46:59 +0100

    Rename dev user and database
    Signed-off-by: Simon Luijk <simon@simonluijk.com>

--- cookbooks/dev_db/recipes/default.rb
+++ cookbooks/dev_db/recipes/default.rb
@@ -11,15 +11,15 @@ connection_info = {:host => node['postgresql']['config']['listen_addresses'],
                    :username => 'postgres',
                    :password => node['postgresql']['password']['postgres']}
 
-postgresql_database 'dev' do
+postgresql_database 'dev_db' do
     connection connection_info
     action :create
 end
 
-postgresql_database_user 'dev' do
+postgresql_database_user 'dev_usr' do
     connection connection_info
     password 'dev_pass'
     action :create
 end
 
-execute "sudo su postgres -c 'psql -c \"ALTER USER dev CREATEDB;\"'"
+execute "sudo su postgres -c 'psql -c \"ALTER USER dev_usr CREATEDB;\"'"
